49a6d73f3c61f6ffec1cf656f2269d7640f00901
Natzaum <natacezerbordignon@gmail.com>
2025-01-02 16:21:00 -0300

    feat: adding getByCpf

---
+++ ms-ticket-manager/src/main/java/desafio3/ms_ticket_manager/service/TicketService.java
@@ -0,0 +1,12 @@
+package desafio3.ms_ticket_manager.service;
+
+import desafio3.ms_ticket_manager.dto.TicketDTO;
+import desafio3.ms_ticket_manager.model.Ticket;
+
+import java.util.List;
+
+public interface TicketService {
+    Ticket createTicket(TicketDTO ticketDTO);
+    Ticket getTicketById(String id);
+    List<Ticket> getTicketByCpf(String cpf);
+}
