136491a344b25903acd782ebd3400afffc5b52dd
Natzaum <natacezerbordignon@gmail.com>
2025-01-06 15:39:04 -0300

    feat: adding .env file for security and fixing minor bugs

--- ms-event-manager/src/main/java/desafio3/ms_event_manager/MsEventManagerApplication.java
+++ ms-event-manager/src/main/java/desafio3/ms_event_manager/MsEventManagerApplication.java
@@ -1,5 +1,6 @@
 package desafio3.ms_event_manager;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -9,7 +10,12 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class MsEventManagerApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+
 		SpringApplication.run(MsEventManagerApplication.class, args);
 	}
-
 }
