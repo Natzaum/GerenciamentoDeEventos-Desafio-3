b74ef3e8d8f80d6ba8c327aa8a27fdc4ec50ef2c
Natzaum <natacezerbordignon@gmail.com>
2025-01-06 14:15:23 -0300

    feat: Adding exception noptFound

---
+++ ms-event-manager/src/main/java/desafio3/ms_event_manager/Exception/EventNotFoundException.java
@@ -0,0 +1,7 @@
+package desafio3.ms_event_manager.Exception;
+
+public class EventNotFoundException extends RuntimeException {
+    public EventNotFoundException(String message) {
+        super(message);
+    }
+}
